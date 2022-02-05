004e1b43c995a07c1050b89429d8433ce068d042
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2022-02-05 13:39:02 +0800

    fix: fix vercel build

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,6 +5,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import WasmPackPlugin from "@wasm-tool/wasm-pack-plugin";
 
 const env = process.env.NODE_ENV as "production" | "development";
+const isProd = env === "production";
 
 console.log(`use ${env} mode`);
 
@@ -56,16 +57,15 @@ const main: WebpackCfg & DevServeCfg = Object.assign({}, base, {
       inject: "body",
       template: resolve(__dirname, "./web/web.html"),
     }),
-    new WasmPackPlugin({
-      crateDirectory: resolve(__dirname, "./wasm"),
-      watchDirectories: [
-        resolve(__dirname, "./wasm/src"),
-        resolve(__dirname, "./wasm/snapshot_parser/src"),
-      ],
-      extraArgs: "--target web --mode normal",
-      forceMode: "production",
-    }),
-  ],
+  ].concat(isProd ? [] :[new WasmPackPlugin({
+    crateDirectory: resolve(__dirname, "./wasm"),
+    watchDirectories: [
+      resolve(__dirname, "./wasm/src"),
+      resolve(__dirname, "./wasm/snapshot_parser/src"),
+    ],
+    extraArgs: "--target web --mode normal",
+    forceMode: "production",
+  })] as any),
 });
 
 const worker: WebpackCfg & DevServeCfg = Object.assign({}, base, {
