9bc774906f2d71dab67ea4d3b79e8b13c04e29c4
zhengrenzhe <zhengrenzhe0416@outlook.com>
2022-12-10 23:56:39 +0800

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Commands {
+    /// analyse local snapshot
+    Local {
+        /// lists test values
+        #[arg(short, long)]
+        file: PathBuf,
+    },
+
+    /// start web workbench
+    Workbench,
+
+    /// realtime analyse Chromium based browser tab v8 heap memory
+    Realtime,
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
 }
