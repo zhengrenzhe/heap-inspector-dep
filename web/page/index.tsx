78a125bac509921fa55222e363f362f4cfffc753
zhengrenzhe <zhengrenzhe0416@outlook.com>
2022-12-24 13:16:33 +0800

    theme toggle

--- web/page/index.tsx
+++ web/page/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "reflect-metadata";
-import { MantineProvider } from "@mantine/core";
+import { ColorScheme, MantineProvider } from "@mantine/core";
 import { configure } from "mobx";
 
 import { Workbench } from "@web/page/workbench";
@@ -15,11 +15,17 @@ const rootDom = document.createElement("div");
 rootDom.id = "app-root";
 document.body.append(rootDom);
 
-const App = (
-  <MantineProvider theme={{ colorScheme: "dark" }}>
-    <Workbench />
-  </MantineProvider>
-);
+function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = () =>
+    setColorScheme(colorScheme === "dark" ? "light" : "dark");
+
+  return (
+    <MantineProvider theme={{ colorScheme }}>
+      <Workbench cs={colorScheme} toggleColorScheme={toggleColorScheme} />
+    </MantineProvider>
+  );
+}
 
 const root = createRoot(rootDom);
-root.render(App);
+root.render(<App />);
--- web/page/workbench.tsx
+++ web/page/workbench.tsx
@@ -1,5 +1,7 @@
+import { ColorScheme, UnstyledButton } from "@mantine/core";
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 import { WorkbenchService } from "@web/page/workbench.service";
 import { inject } from "@web/common";
@@ -8,10 +10,19 @@ import { Canvas } from "@web/page/canvas";
 
 import "./workbench.less";
 
+interface IWorkbenchProps {
+  cs: ColorScheme;
+  toggleColorScheme: () => void;
+}
+
 @observer
-export class Workbench extends Component {
+export class Workbench extends Component<IWorkbenchProps> {
   @inject()
   private wbService: WorkbenchService;
+
+  private get isDark() {
+    return this.props.cs === "dark";
+  }
 
   public override componentDidMount() {
     this.wbService.init();
@@ -19,8 +30,26 @@ export class Workbench extends Component {
 
   public override render() {
     return (
-      <div className="workbench">
+      <div
+        className="workbench"
+        style={{ background: this.isDark ? "#101113" : "#F8F9FA" }}
+      >
         {this.wbService.viewModel.isReady ? <Canvas /> : <Progress />}
+        {this.renderTheme()}
+      </div>
+    );
+  }
+
+  private renderTheme() {
+    return (
+      <div className="theme-toggle">
+        <UnstyledButton
+          c={this.isDark ? "#F8F9FA" : "#101113"}
+          fz={22}
+          onClick={this.props.toggleColorScheme}
+        >
+          {this.isDark ? <MdDarkMode /> : <MdLightMode />}
+        </UnstyledButton>
       </div>
     );
   }
