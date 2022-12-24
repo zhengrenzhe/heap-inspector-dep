eb0dfd2136617c4a335eb0733639671633091ead
zhengrenzhe <zhengrenzhe0416@outlook.com>
2022-12-24 21:31:15 +0800

    vite

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import typescript from "@rollup/plugin-typescript";
+import ttypescript from "ttypescript";
+
+const isProd = process.env["NODE_ENV"] === "production";
+console.log(`is prod: ${isProd}`);
+
+export default defineConfig({
+  root: "./web",
+  resolve: {
+    alias: {
+      "@web": resolve(__dirname, "./web"),
+    },
+  },
+  plugins: [
+    typescript({
+      typescript: ttypescript,
+      sourceMap: !isProd,
+    }),
+  ],
+  build: {
+    outDir: resolve(__dirname, "./dist"),
+    assetsDir: "",
+    emptyOutDir: true,
+  },
+});
