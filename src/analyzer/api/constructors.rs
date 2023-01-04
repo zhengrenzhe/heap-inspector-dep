ad65c14eba08049e977767a61a59ccee7c80b7e0
zhengrenzhe <zhengrenzhe0416@outlook.com>
2023-01-04 22:19:01 +0800

    count

--- src/analyzer/api/constructors.rs
+++ src/analyzer/api/constructors.rs
@@ -1,20 +1,42 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
+
+use serde::Serialize;
 
 use crate::analyzer::consts::{NODE_TYPE_NATIVE, NODE_TYPE_OBJECT};
 use crate::analyzer::Analyzer;
 
+#[derive(Serialize)]
+pub struct ConstructorItem {
+    pub total_self_size: u64,
+    pub count: u64,
+}
+
 impl Analyzer {
-    pub fn constructors(&self) -> HashSet<&String> {
-        let mut hash: HashSet<&String> = HashSet::new();
+    pub fn constructors(&self) -> HashMap<&String, ConstructorItem> {
+        let mut map: HashMap<&String, ConstructorItem> = HashMap::new();
 
         for node in &self.data_provider.nodes {
             let node_type = self.data_provider.get_node_type(node);
             if [NODE_TYPE_OBJECT, NODE_TYPE_NATIVE].contains(&&**node_type) {
                 let node_name = self.data_provider.get_node_name(node);
-                hash.insert(node_name);
+                match map.get_mut(node_name) {
+                    Some(c) => {
+                        c.total_self_size += node.self_size;
+                        c.count += 1;
+                    }
+                    None => {
+                        map.insert(
+                            node_name,
+                            ConstructorItem {
+                                total_self_size: node.self_size,
+                                count: 1,
+                            },
+                        );
+                    }
+                }
             }
         }
 
-        hash
+        map
     }
 }
