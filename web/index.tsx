321406e95edaecc8ddc8f4b81c1927f54e5c5713
zhengrenzhe <zhengrenzhe0416@outlook.com>
2023-03-23 00:00:53 +0800

    ui

---
+++ web/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { Workbench } from "./workbench";
+
+const rootEle = document.getElementById("app");
+
+if (rootEle) {
+  const root = createRoot(rootEle);
+  root.render(<Workbench />);
+}
---
+++ web/workbench.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/joy";
+import React from "react";
+
+export function Workbench() {
+  return (
+    <div>
+      <Button color="primary" variant="soft" size="sm">
+        Hello world
+      </Button>
+    </div>
+  );
+}
